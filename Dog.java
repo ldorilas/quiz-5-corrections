d503f66de2adae0608518eca8d1cdf1832d40d33
ldorilas <43239931+ldorilas@users.noreply.github.com>
2019-01-21 23:56:16 -0500

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,28 +1,55 @@
 import java.util.Scanner;
-/**
-You'll be designing and implementing a class called Dog.
 
-For our purposes, an Dog will be assumed to have a name, breed, color, and weight. While there are certainly many more characteristics of a Dog, we'll only concern ourselves with these.
+public class Dog {
 
-Our Dogs are playful creatures who want to run around and have fun. They can bark and run, and that's it.
+	String name;
+  String breed;
+  String color;
+  double weight;
+  
+  public Dog {
+    this.name = name;
+    this.breed = breed;
+    this.color = color;
+    this.weight = weight;
+  }
 
-When designing your Dog class, each characteristic of Dog should be stored as a class-level variable. Every Dog will get a unique copy of these traits, which means changes to the value of a particular variable in Dog A should not impact Dog B.
+	public void bark() {
 
-Please review the in-depth requirements below, design and implement your class, and upload the .java file to this question.
+		if (breed = "German Shepherd" || breed = "Golden Retriever" || breed = "Rottweiler") {
+      System.out.println("Bark! Bark! Bark!");
+    }
+    else if (breed = "French Bulldog" || breed = "Beagle" || breed = "Yorkshire Terrier") {
+      System.out.println("Bark!");
+    }
+    else {
+      System.out.println("Bark! Bark!");
+    }
+	}
 
-Instance variables with logically chosen data types for each of the following characteristics.
-- Name
-- Breed
-- Color
-- Weight
-- A default constructor that accepts no arguments, but provides default values for each of the aforementioned instance variables.
-- A constructor that accepts starting values for each of the aforementioned instance variables.
-- Getter and setter methods for each of the instance variables.
-- A method called bark that accepts no arguments, and prints a barking message to the console. Larger breeds (for our purposes, these will be limited to German Shepherd, Golden Retriever, and Rottweiler) should bark three times per call to this method. Smaller dog breeds (for our purposes, these will be limited to French Bulldog, Beagle, and Yorkshire Terrier) should bark only once per call to this method. All other breeds should bark twice per call to this method.
-- A method called run that accepts two arguments (i.e., a distance, in steps, and direction in which to move) and prints a reasonable message that reports the distance and direction traveled.
-- A toString method that overrides the default implementation by returning the following:
+	public void run(int steps, String direction) {
 
-`
-"{ Name: name, Breed: breed, Color: color, Weight: weight }"
-`
-*/
+		System.out.println(name + " ran " + steps + " steps toward " + direction + ".");
+
+	}
+  
+  public void toString() {
+    return "Name: " + name + "; Breed: " + breed + "; Color " + color + "; Weight: " + weight;
+  }
+
+	public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Name: ");
+    String name = in.nextLine();
+    System.out.println("Breed: ");
+    String breed = in.nextLine();
+    System.out.println("Color: ");
+    String color = in.nextLine();
+    System.out.println("Weight: ");
+    double weight = in.nextDouble();
+    
+		Dog info = new Dog();
+    info.bark();
+		info.run();
+	}
+}
